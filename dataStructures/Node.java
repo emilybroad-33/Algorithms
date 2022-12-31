7a6a3b54be808c7b7dfd79d41f21ccc3f35100dc
emilybroad-33 <60201748+emilybroad-33@users.noreply.github.com>
2022-12-30 23:41:00 -0500

    Create Node.java

---
+++ dataStructures/Node.java
@@ -0,0 +1,15 @@
+public class Node {
+
+    /*
+    This is used to create the 2 attributes of a Linked List.
+    They're both set to blank (or null) right now so that they always have some sort of value.
+    */
+    String name = null; //item name
+    Node next; //pointer to the next item in the list
+
+    Node(String myName) {
+        this.name = myName;//item name
+        this.next = null;
+    }//Node constructor
+
+}//Node
